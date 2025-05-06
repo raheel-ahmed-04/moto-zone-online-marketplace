e28309b6f9acecde7be71d751ade1b3091ba1c8d
BilalArshad22 <163632482+BilalArshad22@users.noreply.github.com>
2025-05-06 09:13:09 +0500

    Added files for header and footer and some other pages and sections

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import "../../styles/header.css";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/cars",
+    display: "Cars",
+  },
+  {
+    path: "/bikes",
+    display: "Bikes",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__left">
+                <span>Need Help?</span>
+                <span className="header__top__help">
+                  <i className="ri-phone-fill"></i> +92-3105789904
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                <Link to="/login" className="d-flex align-items-center gap-1">
+                  <i className="ri-login-circle-line"></i> Login
+                </Link>
+                <Link to="/register" className="d-flex align-items-center gap-1">
+                  <i className="ri-user-line"></i> Register
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      <div className="header__middle">
+        <Container>
+          <Row className="align-items-center">
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className="d-flex align-items-center gap-2">
+                    <i className="ri-car-line"></i>
+                    <span>
+                      Moto Zone <br /> Showroom
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+
+            <Col lg="4" md="5" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i className="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Pakistan</h4>
+                  <h6>Wah Cantt, Islamabad</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="4" md="4" sm="4" className="d-flex align-items-center justify-content-end">
+              <button className="header__btn btn" style={{ backgroundColor: "#ffc107" }}>
+                <Link to="/contact" style={{ color: "inherit" }}>
+                  <i className="ri-phone-line"></i> Request a call
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      <div className="main__navbar">
+        <Container>
+          <div className="navigation__wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile__menu">
+              <i className="ri-menu-line"></i>
+            </span>
+
+            <div className="navigation">
+              <div className="menu">
+                {navLinks.map((link, index) => (
+                  <NavLink
+                    to={link.path}
+                    key={index}
+                    className="nav__item"
+                    activeClassName="nav__active"
+                  >
+                    {link.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+
+            <div className="search__box">
+              <input type="text" placeholder="Search" />
+              <span>
+                <i className="ri-search-line"></i>
+              </span>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
